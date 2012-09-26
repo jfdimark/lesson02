b66242442d6f26c65cb899a9e2fa9e7577f98375
Mark Walker <m_e_walker@hotmail.com>
2012-09-26 21:38:22 +0100

    lab answers

--- lab.rb
+++ lab.rb
@@ -1,6 +1,14 @@
 # declare a hash representing the four suites of a deck of cards
 # with an abbreviation for the keys and a full name for the values
 
+cards = { hr: "Hearts", ac: "Aces", jk: "Jacks", sp: "Spades"}
+
+cards.each do |key, suit|
+	(1..13).each do |value|
+		puts "#{value} of #{suit}"
+	end
+end
+
 
 # display the rank and suit for each card, as "9 of Hearts", etc.
 # don't worry about "jack" or "king" yet, just go up to "13 of Hearts", etc.
--- loops.rb
+++ loops.rb
@@ -16,8 +16,16 @@ provinces = {
 
 # 1. display each province, followed by its abbreviation, each on its own line
 
+provinces.each do |name, code|
+puts "#{name}: #{code}"
+end
 
-# 2. using the .each method, display the months in the second quarter of the year, in a single row
+# 2. using the .each method, display the months in the second quarter of the year
+second_q = months[3..5]
+
+second_q.each do |month|
+	puts month
+end 
 
 
 # 3. display the current year, followed by the third quarter months, and the same for the next two years, as shown
@@ -27,5 +35,13 @@ provinces = {
 # 2014: July August September
 #
 # there are many different ways you could do this, but you will need to nest one loop inside another.
-# for this exercise, do not use the .each method in either loop
 
+third_q = months[6..8]
+(2012..2014).each do |year|
+	print "#{year}: " 
+		third_q.each do |month|
+			print "#{month} "
+		end
+		#this prints new line
+		print "\n"
+end
