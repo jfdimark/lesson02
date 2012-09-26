1e840be4693718bcb689a49668a737f92cbbcbcf
Mark Walker <m_e_walker@hotmail.com>
2012-09-26 19:44:06 +0100

    Arrays answers

--- arrays.rb
+++ arrays.rb
@@ -1,8 +1,18 @@
 # 1. create an array with three different string values
 
+three_amigos = ["John", "Tom", "Bob"]
+
 # 2. add two new values to the Task #1 array in single expression
+
+puts three_amigos.unshift("Jason", "Jim")
 
 # 3. convert your name to an array, and display it, in a single expression
 
+puts ["Mark", "Walker"]
+
 # 4. remove and display the last value in the Task # 1 array, and discuss
 # with the instructor why both values added in Task # 2 may have been removed
+
+he_went = three_amigos.pop
+
+puts he_went
